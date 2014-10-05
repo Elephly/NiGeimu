ae9f8f718ea7c45b93b380acd23f8f44c200adda
Nick Chiasson <elephly@live.com>
2014-10-05 17:25:28 -0400

    Update
    Shelly

---
+++ ニゲーム/ニゲーム/Fighter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ニゲーム
+{
+    class Fighter
+    {
+        private Animation animation;
+
+        public Fighter()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
