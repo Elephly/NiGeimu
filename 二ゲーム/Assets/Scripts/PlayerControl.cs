d8abb219485c62695f7303aa775454f26d5b49fc
shababislam <smi.zak@gmail.com>
2014-10-08 16:23:11 -0400

    Update
    added jump

--- 二ゲーム/Assets/Scripts/PlayerControl.cs
+++ 二ゲーム/Assets/Scripts/PlayerControl.cs
@@ -4,30 +4,37 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	float speed = 8f;
-	float jumpSpeed = 15f;
-
-	bool grounded = true;
+	float jumpSpeed = 280f;
+	
+	bool canJump;
+	bool isJumping;
 
 	void Update () {
 		Movement();
 	}
 
 	void Movement () {
-		if (grounded){
-			if (Input.GetKey(KeyCode.D)){
-				transform.Translate (Vector2.right * speed *Time.deltaTime);
-				transform.eulerAngles = new Vector2(0,0);
-			}
-			if (Input.GetKey(KeyCode.A)){
-				transform.Translate (Vector2.right * speed *Time.deltaTime);
-				transform.eulerAngles = new Vector2(0,180);			
-			}
-			if (Input.GetKey(KeyCode.Space)){
-				rigidbody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
 
-			}
+		if (Input.GetKey(KeyCode.D)){
+			transform.Translate (Vector2.right * speed *Time.deltaTime);
+			transform.eulerAngles = new Vector2(0,0);
 		}
+		if (Input.GetKey(KeyCode.A)){
+			transform.Translate (Vector2.right * speed *Time.deltaTime);
+			transform.eulerAngles = new Vector2(0,180);			
+		}
+		if (canJump && Input.GetKeyDown(KeyCode.Space)){
+			rigidbody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+			canJump = false;
+			isJumping = true;
+		}
+	}
 
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "ground"){
+			canJump = true;
+			isJumping = false;
+		}
 	}
 
 
