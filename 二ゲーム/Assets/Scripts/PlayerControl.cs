05bca6b8aef918ec3e5250fbec44f7758082bc7d
shababislam <smi.zak@gmail.com>
2014-10-07 20:47:23 -0400

    Movement
    movement + sprite/stand cycle

---
+++ 二ゲーム/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	float speed = 8f;
+	float jumpSpeed = 15f;
+
+	bool grounded = true;
+
+	void Update () {
+		Movement();
+	}
+
+	void Movement () {
+		if (grounded){
+			if (Input.GetKey(KeyCode.D)){
+				transform.Translate (Vector2.right * speed *Time.deltaTime);
+				transform.eulerAngles = new Vector2(0,0);
+			}
+			if (Input.GetKey(KeyCode.A)){
+				transform.Translate (Vector2.right * speed *Time.deltaTime);
+				transform.eulerAngles = new Vector2(0,180);
+			
+			}
+
+			if (Input.GetKey(KeyCode.Space)){
+				rigidbody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+
+			}
+		}
+
+	}
+}
